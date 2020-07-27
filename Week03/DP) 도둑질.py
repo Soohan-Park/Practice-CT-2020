a54590e61c0462ae7e9e105198f4a4e959f78cd6
Soohan Park <725psh@gmail.com>
2020-07-27 23:11:04 +0900

    CT 2020 - Week 03 (5)

---
+++ Week03/DP) 도둑질.py
@@ -0,0 +1,33 @@
+# 흠..
+
+def solution(money:list):
+    answer = 0
+
+    length = len(money)
+    for i in range(len(money)):
+        house = money.copy()
+        stolen = house[i]
+
+        house[i] = -1
+        house[(i-1) % length] = -1
+        house[(i+1) % length] = -1
+
+        target = max(house)
+        while target >= 0:
+            stolen += target
+
+            idx = house.index(target)
+            house[idx] = -1
+            house[(idx-1) % length] = -1
+            house[(idx+1) % length] = -1
+
+            target = max(house)
+
+        if stolen > answer:
+            answer = stolen
+
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([1, 2, 3, 1]))
