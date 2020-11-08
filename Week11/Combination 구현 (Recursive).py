6ebd2ec987ed8ca50ed833dad42f564f19bf83f5
Soohan Park <725psh@gmail.com>
2020-11-08 15:28:58 +0900

    CT 2020 - Week11 (6) : Add Comment (Combination)

--- Week11/Combination 구현 (Recursive).py
+++ Week11/Combination 구현 (Recursive).py
@@ -18,14 +18,14 @@ def combi(before, arr, answer):
     arr[0] = '@'
 
     for i in range(len(arr)):
-        if arr[i] == '@':
+        if arr[i] == '@':  # 끝을 나타내는 문자를 만난 경우
             if target not in answer:
                 answer.append(target)
         else:
             spam = target + arr[i]
             if spam not in answer:
                 answer.append(spam)
-            combi(target, arr[i:], answer)
+            combi(target, arr[i:], answer)  # Recursive
 
     return answer
 
@@ -36,10 +36,10 @@ if __name__ == '__main__':
     target = ['A', 'A', 'B', 'C']
     answer = []
     for _ in range(len(target)):
-        answer = combi('', target, answer)
-        target = target[1:]
+        answer = combi('', target, answer)  # answer에 계속 이어서 추가
+        target = target[1:]  # 순열이 아닌 조합이므로, C-A 같은 경우를 제외하기 위해
 
-    answer.sort(key=lambda x : len(x))
+    answer.sort(key=lambda x : len(x))  # 길이 순으로 정렬
 
     for ans in answer:
         print([x for x in ans])
