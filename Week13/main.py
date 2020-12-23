d3461e6f1b8791306c5022e05430711e60b48870
Soohan Park <725psh@gmail.com>
2020-12-23 23:33:18 +0900

    CT 2020 - Week13 (0) : main.py

---
+++ Week13/main.py
@@ -0,0 +1,22 @@
+"""
+BAEKJOON CODES
+  - 각 문제별로 파일을 만들고, 이를 모듈 형태로 제작.
+  - 각 문제 파일을 import하고, 각각에 대한 run()을 호출.
+"""
+
+# PROBLEMS
+# from problems import _PROBLEM_ as prob
+
+# from problems import P2138 as prob
+# from problems import P9250 as prob
+# from problems import P15688 as prob
+# from problems import P16198 as prob
+
+
+# DO_NOT_TOUCH
+def main():
+  prob.run()
+
+
+if __name__ == '__main__':
+  main()
